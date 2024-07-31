2743602d4beb5252cb033f38e0b15a64d1e8adb8
Tom Griffiths <tom.griffiths@esb.ie>
2024-07-31 14:47:35 +0100

    Update split_url

--- backend/utils.py
+++ backend/utils.py
@@ -97,8 +97,11 @@ def generate_SAS(url):
     return sas_token
 
 def split_url(url):
-    pattern = fr'{BLOB_ACCOUNT}/([\w-]+)/([\w-]+\.\w+)'
-    match = re.search(pattern, url)
+    url_decoded = unquote(url)
+    if url_decoded.endswith('/'):
+        url_decoded = url_decoded[:-1]
+    pattern = fr"{BLOB_ACCOUNT}/([^/]+)/(.+)"
+    match = re.search(pattern, url_decoded)
     container = match.group(1)
     blob = match.group(2)
     return container, blob
