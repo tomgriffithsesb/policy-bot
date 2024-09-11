809b6ffaee866f613347cd4dcc53691263a5f0cf
Tom Griffiths <tom.griffiths@esb.ie>
2024-09-11 15:11:24 +0100

    Update answer parser to handle encoding file titles

--- backend/utils.py
+++ backend/utils.py
@@ -136,7 +136,7 @@ def preprocess_response(content,no_data_response):
     Preprocessing of bot response before it is returned. Following preprocessing is performed:
     - Rewrite message returned when AI Search is unable to find data
     '''
-    content = content.replace("The requested information is not found in the retrieved data. Please try another query or topic.",no_data_response)
+    content = content.replace("The requested information is not found in the retrieved data. Please try another query or topic.",no_data_response).replace(r"\u20ac","€")
     return content
 
 def format_non_streaming_response(chatCompletion, history_metadata, apim_request_id):
